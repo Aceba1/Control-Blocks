bd009ea5ef3d54e058835c7c2b571e5d05349e6b
Aceba1 <aceba1@outlook.com>
2019-01-27 10:27:44 -0500

    Update velocity aim

--- Control Block/SwivelBlock.cs
+++ Control Block/SwivelBlock.cs
@@ -122,7 +122,7 @@ namespace Control_Block
                             CurrentAngle = parts[parts.Length - 1].localRotation.eulerAngles.y;
                             break;
                         case Mode.AimAtVelocity:
-                            gimbal.AimFree(Quaternion.Inverse(block.transform.rotation) * tankcache.rbody.velocity + block.transform.forward * 0.02f, (RotateSpeed / Time.deltaTime));
+                            gimbal.Aim(block.transform.position + block.transform.InverseTransformDirection(tankcache.rbody.velocity) + block.transform.forward * 0.01f, (RotateSpeed / Time.deltaTime));
                             CurrentAngle = parts[parts.Length - 1].localRotation.eulerAngles.y;
                             break;
 
