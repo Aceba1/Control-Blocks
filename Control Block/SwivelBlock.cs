2e71b55801548b8c6554f6886c16d42d96886ce7
Aceba1 <aceba1@outlook.com>
2019-01-27 12:12:31 -0500

    Modify velocities yet again

--- Control Block/SwivelBlock.cs
+++ Control Block/SwivelBlock.cs
@@ -122,7 +122,7 @@ namespace Control_Block
                             CurrentAngle = parts[parts.Length - 1].localRotation.eulerAngles.y;
                             break;
                         case Mode.AimAtVelocity:
-                            gimbal.Aim(block.transform.position - tankcache.rbody.velocity + block.transform.forward * 0.15f, (RotateSpeed / Time.deltaTime));
+                            gimbal.Aim(block.transform.position + tankcache.rbody.velocity + block.transform.forward * 1f, (RotateSpeed / Time.deltaTime));
                             CurrentAngle = parts[parts.Length - 1].localRotation.eulerAngles.y;
                             break;
 
