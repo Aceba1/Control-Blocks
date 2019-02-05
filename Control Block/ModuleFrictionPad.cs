8e9b568b1073dcf7d63b49bdea2cfb78bfbe48bc
Aceba1 <aceba1@outlook.com>
2019-02-05 12:11:31 -0500

    Increase traction velocity resolution

--- Control Block/ModuleFrictionPad.cs
+++ Control Block/ModuleFrictionPad.cs
@@ -29,7 +29,7 @@ namespace Control_Block
                         block.tank.transform.position += force;
                         var pos = block.transform.position + GetEffector(block);
                         var rbody = block.tank.rbody;
-                        block.tank.rbody.AddForceAtPosition((/*Vector3.ProjectOnPlane(-rbody.GetPointVelocity(pos), block.transform.rotation * Vector3.up) * strength +*/ force) * 10 + block.transform.rotation * Vector3.down * threshold * 8, pos, ForceMode.Impulse);
+                        block.tank.rbody.AddForceAtPosition((/*Vector3.ProjectOnPlane(-rbody.GetPointVelocity(pos), block.transform.rotation * Vector3.up) * strength +*/ force) * 20 + block.transform.rotation * Vector3.down * threshold * 12, pos, ForceMode.Impulse);
                     }
                     //else force = force.normalized;
                     //block.tank.rbody.AddForceAtPosition(force * 30f, block.transform.position + (block.transform.rotation * Vector3.up * 0.5f), ForceMode.VelocityChange);
@@ -40,7 +40,7 @@ namespace Control_Block
         Vector3 LastPos;
         void FixedUpdate()
         {
-            var block = gameObject.GetComponent<TankBlock>();
+            //var block = gameObject.GetComponent<TankBlock>();
             DoIt = true;
         }
     }
