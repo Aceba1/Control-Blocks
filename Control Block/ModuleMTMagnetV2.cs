3302f5bd116ed86a6ff63b5c13a05a172ba3fff5
Arniel <aceba1@outlook.com>
2020-07-16 15:55:40 -0400

    Prepare for ModuleMTMagnet revamp

---
+++ Control Block/ModuleMTMagnetV2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Control_Block
+{
+    class ModuleMTMagnetV2 : Module
+    {
+        Collider[] checkCount;
+        public Vector3 Effector;
+        public Vector3 CheckCenter;
+        public float CheckRadius;
+        void OnPool()
+        {
+            checkCount = new Collider[8];
+        }
+
+        void FixedUpdate()
+        {
+            if (block.IsAttached && block.tank != null)
+            {
+                int count = Physics.OverlapSphereNonAlloc(CheckCenter, CheckRadius, checkCount, Globals.inst.layerTank.mask, QueryTriggerInteraction.Ignore);
+
+            }
+        }
+    }
+}
